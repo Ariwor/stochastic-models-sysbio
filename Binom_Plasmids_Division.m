0b9fada0ea34c34de57d7b2acc1c0f5bfa49ab51
Ariwor <aarampatzis@student.ethz.ch>
2020-04-16 22:59:12 +0300

    Intro to random variables & stochastic processes

---
+++ Binom_Plasmids_Division.m
@@ -0,0 +1,38 @@
+% This code demonstrates a biological example/phenomenon that gives rise to the binomial distribution.
+% A mother cell (with m plasmids) divides, giving rise to two daughter cells. X is a random variable
+% that is defined as the number of plasmids that are inherited by the first daughter cell (with probability p)
+% after division. It is demonstrated that X follows a binomial distribution (m,p).
+
+%% Set up number of plasmids m and probability p at time of division
+m = 10;
+p = 0.6;
+
+% Generate random variables u, uniformly distributed over [0,1]
+u = rand(1,m);
+
+% Calculate number of plasmids X
+X = sum(u<p);
+
+%% Compute n = 10000 realizations of X
+n = 10000;
+X = zeros(1, 10000);
+for i=1:n
+    u = rand(1,m);
+    X(i) = sum(u<p);
+end
+
+% Plotting
+tiledlayout(2,1)
+nexttile
+histogram(X,'Normalization','probability', 'FaceColor','#77AC30');
+title('Histogram of random variable X');
+
+% Plotting the p.m.f of the Binomial distribution
+x = [0:10];
+pmf_b = binopdf(x,m,p);
+nexttile
+scatter(x,pmf_b,'r');
+title('Probability mass function of the Binomial distribution');
+
+
+
---
+++ CLT_demo.m
@@ -0,0 +1,57 @@
+% This code demonstrates the Central Limit Theorem. X is a continuous random variable
+% with logistic distribution. The aim is to show that the distribution of Z
+% (defined in the code) approaches the standard normal distribution, with increasing n.
+%% Compute one realization of Z with a desired n value
+n = 100;
+X = zeros(1, 100);
+for i=1:n
+    u = rand(1); % generate u from the uniform distribution
+    X(i) = 2 - log((1-u)/u)*sqrt(3); % inverse function for an example c.d.f. (of the logistic distribution)
+    Z = (sum(X) - 2*n)/(pi*sqrt(n));
+end
+%% Compute m realizations of Z with n = 100
+n = 100;
+m = 1000;
+X = zeros(1, 100);
+Z = zeros(1, 1000);
+for j=1:m
+for i=1:n
+    u = rand(1); % generate u from the uniform distribution
+    X(i) = 2 - log((1-u)/u)*sqrt(3); % inverse function for an example c.d.f. (of the logistic distribution)
+end
+    Z(j) = (sum(X) - 2*n)/(pi*sqrt(n));
+end
+
+% Plotting
+edges = [-10,10];
+h1 = histogram(Z, edges, 'Normalization','probability', 'FaceColor','#77AC30');
+h1.BinWidth = 1;
+title('Histogram of random variable Z');
+%% Compute m realizations of Z with n = [100, 1000, 10000]
+n_v = [100, 1000, 10000];
+m = 1000;
+X = zeros(1, 10000);
+Z = zeros(1, 1000);
+for n = n_v
+for j=1:m
+    for i=1:n
+        u = rand(1); % generate u from the uniform distribution
+        X(i) = 2 - log((1-u)/u)*sqrt(3); % inverse function for an example c.d.f. (of the logistic distribution)
+    end
+    Z(j) = (sum(X) - 2*n)/(pi*sqrt(n));
+end
+
+% Plotting
+figure();
+tiledlayout(2,1)
+nexttile
+edges = [-10,10];
+h1 = histogram(Z, edges, 'Normalization','probability', 'FaceColor','#77AC30');
+h1.BinWidth = 1;
+
+% Plotting the p.d.f. of a standard normal distribution
+x = [-10:.1:10];
+pdf_n = normpdf(x);
+nexttile
+plot(x, pdf_n, 'r');
+end
